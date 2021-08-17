f6a0b9a631e18862ccb126596a31c88107b965c8
fauxy99 <fauzyhammawa@gmail.com>
2021-08-17 22:44:59 +0100

    Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import FAQs from '../components/faqs'
 import Hero from '../components/Hero'
 import HeroPlus from '../components/HeroPlus'
 import MaxFooter from '../components/MaxFooter'
